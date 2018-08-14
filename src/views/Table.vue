9698c815a063acfff73ebb7eb97aeac0e1b5384d
onesimu <872634214@qq.com>
2018-08-14 12:48:15 +0800

    渲染表格

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -41,182 +41,13 @@
                 <td>Rhythm Guitar</td>
                 <td class="is-icon">
                   <a href="#">
-                    <i class="fa fa-twitter"></i>
-                  </a>
-                </td>
-                <td class="is-icon">
-                  <a href="#">
-                    <i class="fa fa-instagram"></i>
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td>Robert Mikels</td>
-                <td>Lead Guitar</td>
-                <td class="is-icon">
-                  <a href="#">
-                    <i class="fa fa-twitter"></i>
-                  </a>
-                </td>
-                <td class="is-icon">
-                  <a href="#">
-                    <i class="fa fa-instagram"></i>
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td>Karyn Holmberg</td>
-                <td>Drums</td>
-                <td class="is-icon">
-                  <a href="#">
-                    <i class="fa fa-twitter"></i>
-                  </a>
-                </td>
-                <td class="is-icon">
-                  <a href="#">
-                    <i class="fa fa-instagram"></i>
-                  </a>
-                </td>
-              </tr>
+
             </tbody>
           </table>
         </article>
       </div>
     </div>
 
-    <div class="tile is-ancestor">
-      <div class="tile is-parent">
-        <article class="tile is-child box">
-          <h4 class="title">Table Responsive</h4>
-          <div class="table-responsive">
-            <table class="table is-striped is-narrow">
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Open source projects</th>
-                  <th>Year started</th>
-                  <th colspan="3">Links</th>
-                </tr>
-              </thead>
-              <tfoot>
-                <tr>
-                  <th></th>
-                  <th>Open source projects</th>
-                  <th>Year started</th>
-                  <th colspan="3">Links</th>
-                </tr>
-              </tfoot>
-              <tbody>
-                <tr>
-                  <td class="is-icon">
-                    <i class="fa fa-android"></i>
-                  </td>
-                  <td>
-                    <a href="#">Android</a>
-                  </td>
-                  <td>
-                    2003
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-github"></i>
-                    </a>
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-twitter"></i>
-                    </a>
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-globe"></i>
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="is-icon">
-                    <i class="fa fa-firefox"></i>
-                  </td>
-                  <td>
-                    <a href="#">Firefox</a>
-                  </td>
-                  <td>
-                    2003
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-github"></i>
-                    </a>
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-twitter"></i>
-                    </a>
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-globe"></i>
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="is-icon">
-                    <i class="fa fa-linux"></i>
-                  </td>
-                  <td>
-                    <a href="#">Linux</a>
-                  </td>
-                  <td>
-                    2003
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-github"></i>
-                    </a>
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-twitter"></i>
-                    </a>
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-globe"></i>
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="is-icon">
-                    <i class="fa fa-wordpress"></i>
-                  </td>
-                  <td>
-                    <a href="#">WordPress</a>
-                  </td>
-                  <td>
-                    2003
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-github"></i>
-                    </a>
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-twitter"></i>
-                    </a>
-                  </td>
-                  <td class="is-icon">
-                    <a href="#">
-                      <i class="fa fa-globe"></i>
-                    </a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </article>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -225,14 +56,7 @@ export default {
 
   data () {
     return {
-      data: {
-        body: {
-          username: null,
-          password: null
-        },
-        rememberMe: false
-      },
-      error: null
+			seaexpressprices:[]
     }
   },
   async mounted () {
@@ -246,9 +70,11 @@ export default {
 		// var data = await this.$spring.get('/ports').follow('next').then(json => console.log(this.$spring.Port.jsonToEntityList(json)));
 		var data = await this.$spring.Port.findAll().then(
 			// json => console.log(json[0].get(nameCn))
-			json => console.log(json)
+			json => this.seaexpressprices = json.map(it => it.data())
 		)
-		console.log(data);
+// 		var data = await this.$spring.Seaexpressprice.findAll()
+// 		this.seaexpressprices = data
+		console.log('data'+data);
   },
   methods: {
 

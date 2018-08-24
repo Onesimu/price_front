dd755d4abc1972eb21b014bfc07162cacae1195a
onesimu <872634214@qq.com>
2018-08-24 15:28:36 +0800

    关于页面添加图片和公司信息

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,7 +1,36 @@
 <template>
 	<section>
 		<figure class="image">
-			<img src="../assets/banner.jpg">
+			<img src="../assets/about_banner.png">
+		</figure>
+
+		<div class="box is-offset-3 is-6">
+			<div class="content has-text-centered">
+				<h1 class="title is-2">联系我们</h1>
+				<p class="social is-large">
+					<h2 class="subtitle">信息技术有限公司</h2>
+					地址：河北石家庄
+					<br> 邮编：05000
+					<br> 电话：400-004-0000
+					<br> 传真：002-8888888、020-58393074
+					<br> 邮箱：market@sina.cn
+				</p>
+			</div>
+		</div>
+
+		<figure class="image-fluent">
+			<img src="../assets/banner4.png">
 		</figure>
 	</section>
 </template>
+
+<style scoped>
+	.has-margin-3 {
+		margin-top: 1rem;
+	}
+
+	.is-large {
+		font-size: 2.5rem;
+		line-height: 3rem
+	}
+</style>
